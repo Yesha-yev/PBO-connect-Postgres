e8586067e449332808f607079b1b79aefea19ef5
Mita <242410103084@mail.unej.ac.id>
2025-05-09 13:45:50 +0700

    PBO

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PBO_connect_Postgres
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            gridToDo = new DataGridView();
+            BoxToDo = new TextBox();
+            Simpan = new Button();
+            ((System.ComponentModel.ISupportInitialize)gridToDo).BeginInit();
+            SuspendLayout();
+            // 
+            // gridToDo
+            // 
+            gridToDo.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gridToDo.Location = new Point(12, 12);
+            gridToDo.Name = "gridToDo";
+            gridToDo.Size = new Size(480, 215);
+            gridToDo.TabIndex = 0;
+            // 
+            // BoxToDo
+            // 
+            BoxToDo.Location = new Point(12, 245);
+            BoxToDo.Multiline = true;
+            BoxToDo.Name = "BoxToDo";
+            BoxToDo.Size = new Size(480, 23);
+            BoxToDo.TabIndex = 1;
+            // 
+            // Simpan
+            // 
+            Simpan.BackColor = Color.FromArgb(64, 0, 0);
+            Simpan.ForeColor = SystemColors.HighlightText;
+            Simpan.Location = new Point(12, 288);
+            Simpan.Name = "Simpan";
+            Simpan.Size = new Size(75, 23);
+            Simpan.TabIndex = 2;
+            Simpan.Text = "SIMPAN";
+            Simpan.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Simpan);
+            Controls.Add(BoxToDo);
+            Controls.Add(gridToDo);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)gridToDo).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView gridToDo;
+        private TextBox BoxToDo;
+        private Button Simpan;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace PBO_connect_Postgres
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
